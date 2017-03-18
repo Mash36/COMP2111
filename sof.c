948a77a6fa7adbfa9ece0ce689dc9d8fed345443
Mash36 <mashiur@live.com>
2017-03-18 16:05:59 +1100

    made code even shorter

--- sof.c
+++ sof.c
@@ -8,14 +8,9 @@ unsigned long sof(unsigned long t, unsigned long a, unsigned long b) {
 	unsigned long result = 0;
 	
 	while (b <= t) {
-		if (b % 2 != 0)
-		{
-			result += b;
-		}
+		if (b % 2 != 0) result += b;
 		b += a;
 		a = b - a;
 	}
 	return result;
 }
-
-//Done
